095b50bdbc4e6d17f421134fe6817218e7c7020e
Joshua Derocher <josh@vlkpack.com>
2023-03-18 15:17:51 -0400

    monogodb

---
+++ src/netlify/functions/create.ts
@@ -0,0 +1,31 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+exports.handler = async () => {
+  const password = process.env.MONGO_PASSWORD;
+
+  const uri = `mongodb+srv://picardnumber1:${password}@cluster0.ypureou.mongodb.net/?retryWrites=true&w=majority`;
+
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+
+  client
+    .connect((err) => {
+      const collection = client.db("test").collection("devices");
+      // perform actions on the collection object
+
+      console.log(collection);
+
+      // collection.add
+
+      client.close();
+    })
+    .then(console.log)
+    .catch(console.error);
+
+  return {
+    status: 200,
+  };
+};
