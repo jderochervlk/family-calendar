e9d8affbe76c061d69afb7f1ee61b22aaf0dd26c
Joshua Derocher <josh@vlkpack.com>
2023-03-18 14:04:08 -0400

    setup datastax

---
+++ src/netlify/functions/create.js
@@ -0,0 +1,46 @@
+const { createClient } = require("@astrajs/collections");
+
+
+let astraClient = null;
+
+const getAstraDocClient = async () => {
+    if (astraClient === null) {
+        astraClient = await createClient(
+            {
+                astraDatabaseId: process.env.ASTRA_DB_ID,
+                astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+                applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
+            },
+            30000
+        );
+    }
+    return astraClient;
+};
+
+async function getCollection() {
+    const documentClient = await getAstraDocClient();
+    return documentClient
+        .namespace('todos')
+        .collection("doc");
+};
+
+exports.handler = async (event, context) => {
+    const todos = await getCollection();
+    const body = { id: "foo-bar-101" };
+
+    try {
+        const res = await todos.create(body.id, "hey there!")
+        return {
+            statusCode: 201,
+            body: JSON.stringify(res),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        };
+    } catch (e) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify(e),
+        };
+    }
+};
